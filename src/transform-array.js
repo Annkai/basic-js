408fb392a022305d8c760c19493f97ce463d8ab0
Annkai <kaimol@inbox.ru>
2022-10-14 22:21:19 +0300

    feat: add transform array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,37 +14,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function transform(mas) {
-    throw new NotImplementedError('Not implemented');
-
-    let arr = mas.slice();
-    let result;
-
-
+    //throw new NotImplementedError('Not implemented');
     if (!Array.isArray(mas)) {
         throw new Error("'arr' parameter must be an instance of the Array!");
-    } else {
-
+    }
+    let arr = mas.slice();
+    let result;
+    let discardPrev = arr.indexOf('--discard-prev');
+    let doublePrev = arr.indexOf('--double-prev');
+    let discardNext= arr.indexOf('--discard-next');
+ 
+          if (discardNext === doublePrev - 2) {
+            arr.splice(discardNext, 2);
+            arr.splice(doublePrev - 2, 1);
+          } else if (discardNext === discardPrev - 2) {
+            arr.splice(discardNext, 2);
+            arr.splice(discardPrev - 2, 1);
+          }
+    
         for (let i = 0; i < arr.length; i++) {
-
-            if (arr[i] === '--double-next') {
+            if (arr[i - 1] === undefined && arr[i] === '--discard-prev') {
+                arr.splice(i, 1);
+            } else if (arr[i - 1] === undefined && arr[i] === '--double-prev') {
+                arr.splice(i, 1);
+            } else if (arr[i + 1] === undefined && arr[i] === '--double-next') {
+                arr.splice(i, 1);
+            } else if (arr[i + 1] === undefined && arr[i] === '--discard-next') {
+                arr.splice(i, 1);
+            } else if (i !== arr.length - 1 && arr[i] === '--double-next') {
                 arr[i] = arr[i + 1];
-            } else if (arr[i] === '--double-prev') {
+            } else if (i !== 0 && arr[i] === '--double-prev') {
                 arr[i] = arr[i - 1];
-            } else if (arr[i] === '--discard-prev') {
+            } else if (i !== 0 + 1 && arr[i] === '--discard-prev') {
                 arr.splice(i - 1, 2);
-            } else if (arr[i] === '--discard-next') {
+            } else if (i !== arr.length - 1 && arr[i] === '--discard-next') {
                 arr.splice(i, 2);
             }
-
         }
         result = arr;
-    }
-
-
-
-    return result;
-
-
+        return result;
 }
 
 module.exports = {
