acac62e7c38aa0905aa52a9a0a811db8238b5ec4
Annkai <kaimol@inbox.ru>
2022-10-17 10:38:02 +0200

    feat: add vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,16 +20,107 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+
+    constructor(value = true) {
+        this.value = value;
+        this.alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+        this.keywordKeys = [];
+        this.stringKeys = [];
+        this.newKeys = [];
+        this.arr = [];
+    }
+
+    code(string, keyword) {
+        this.keywordKeys = [];
+        this.stringKeys = [];
+        this.newKeys = [];
+        this.arr = [];
+
+        if (!string || !keyword) {
+            throw new Error('Incorrect arguments!');
+        } else {
+            keyword = keyword.toUpperCase();
+            string = string.toUpperCase();
+
+            for (let n = 0; n < string.length; n++) {
+                if (this.alphabet.indexOf(string[n]) === -1) {
+                    this.arr.push(n);
+                } else {
+                    this.stringKeys.push(this.alphabet.indexOf(string[n]));
+                }
+            }
+
+            while (this.keywordKeys.length < this.stringKeys.length) {
+                for (let i = 0; i < keyword.length; i++) {
+                    this.keywordKeys.push(this.alphabet.indexOf(keyword[i]));
+                }
+            }
+
+            for (let v = 0; v < this.stringKeys.length; v++) {
+                this.newKeys.push(this.alphabet[(this.keywordKeys[v] + this.stringKeys[v]) % this.alphabet.length]);
+            }
+
+            //insert the cut elements back
+            for (let i = 0; i < this.arr.length; i++) {
+                this.newKeys.splice(this.arr[i], 0, string[this.arr[i]]);
+            }
+            return this.value ? this.newKeys.join('') : this.newKeys.reverse().join('');
+        }
+    }
+
+    decode(string, keyword) {
+        this.keywordKeys = [];
+        this.stringKeys = [];
+        this.newKeys = [];
+        this.arr = [];
+
+        if (!string || !keyword) {
+            throw new Error('Incorrect arguments!');
+        } else {
+            keyword = keyword.toUpperCase();
+            string = string.toUpperCase();
+
+            for (let n = 0; n < string.length; n++) {
+                if (this.alphabet.indexOf(string[n]) === -1) {
+                    this.arr.push(n);
+                } else {
+                    this.stringKeys.push(this.alphabet.indexOf(string[n]));
+                }
+            }
+
+            while (this.keywordKeys.length < this.stringKeys.length) {
+                for (let i = 0; i < keyword.length; i++) {
+                    this.keywordKeys.push(this.alphabet.indexOf(keyword[i]));
+                }
+            }
+
+            for (let v = 0; v < this.stringKeys.length; v++) {
+                if (this.stringKeys[v] - this.keywordKeys[v] < 0) {
+                    this.newKeys.push(this.alphabet[(this.stringKeys[v] - this.keywordKeys[v] + this.alphabet.length) % this.alphabet.length]);
+                } else {
+                    this.newKeys.push(this.alphabet[this.stringKeys[v] - this.keywordKeys[v]]);
+                }
+            }
+
+            //insert the cut elements back
+            for (let i = 0; i < this.arr.length; i++) {
+                this.newKeys.splice(this.arr[i], 0, string[this.arr[i]]);
+            }
+            return this.value ? this.newKeys.join('') : this.newKeys.reverse().join('');
+        }
+    }
+
+    encrypt(string, keyword) {
+        //throw new NotImplementedError('Not implemented');
+        return this.code(string, keyword);
+    }
+
+    decrypt(string, keyword) {
+        //throw new NotImplementedError('Not implemented');
+        return this.decode(string, keyword);
+    }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+    VigenereCipheringMachine
 };
